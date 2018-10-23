8ed6865e7d19a8bebc7681c3801db4f5eec45721
Anderson Taylor <andltayl@ut.utm.edu>
2018-10-23 16:11:05 -0500

    WIP

---
+++ Threading/FindPiThread.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    class FindPiThread
+    {
+        int numDarts, goodDarts;
+        Random r;
+
+        public FindPiThread(int numDarts)
+        {
+            this.numDarts = numDarts;
+            this.goodDarts = 0;
+            this.r = new Random();
+        }
+
+        public int GetDarts()
+        {
+            return goodDarts;
+        }
+
+        public void throwDarts()
+        {
+            for(int i = 0; i < numDarts; i++)
+            {
+                double x, y;
+
+                x = r.NextDouble();
+                y = r.NextDouble();
+
+                if((x*x + y*y) <= 1)
+                {
+                    goodDarts++;
+                }
+            }
+        }
+    }
+    
+
+}
--- Threading/Program.cs
+++ Threading/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Threading
 {
@@ -10,6 +11,23 @@ namespace Threading
     {
         static void Main(string[] args)
         {
+            int throws, numThreads;
+            List<Thread> threads;
+            List<FindPiThread> darts;           
+
+            Console.WriteLine("How many throws per thread? ");
+            throws = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many threads? ");
+            numThreads = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numThreads; i++)
+            {
+                FindPiThread temp = new FindPiThread(throws);
+
+                darts.Add(temp);
+                     
+            }
         }
     }
 }
